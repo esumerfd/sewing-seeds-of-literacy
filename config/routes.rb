93960cbbbd6210eaa07ab34436354e7894ea06e2
Bill Barnett <bbarnett@maggie.local>
2009-03-04 14:55:32 -0500

    Added Restful Authentication.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 ActionController::Routing::Routes.draw do |map|
+
+  map.resources :users
+  map.resource :session
+
+  map.root :controller => 'visitor'
+
+  map.activate '/activate/:activation_code', :controller => 'users', :action => 'activate'
+  map.signup '/signup', :controller => 'users', :action => 'new'
+  map.login '/login', :controller => 'sessions', :action => 'new'
+  map.logout '/logout', :controller => 'sessions', :action => 'destroy'
+
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
